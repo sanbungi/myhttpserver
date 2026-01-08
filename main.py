6d19128d15542b7edcca98b829c3f86188c9ab02
sanbungi <0312pika0201@gmail.com>
2026-01-09 05:44:31 +0900

    基礎的なルーティングWIP

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
+import os
 import socket
 from pprint import pprint
 from pathlib import Path
 from utils import HTTPRequest, parse_request
 
 
-def make_response(path: str = ""):
-    with open("html/index.html", "r", encoding="utf-8") as f:
-        content = f.read()
-        # pprint(content)
+def make_response(filepath: str = "."):
+    path = Path(filepath)
+    print(f'path is :{path}')
 
-    return content, len(content)
+    # pathがrootならindexを返す
+    if path == Path("/"):
+        with open(f"html/index.html", "r", encoding="utf-8") as f:
+            content = f.read()
+            print("index.html")
+            return content, len(content)
+    
+    tmp = 'html' + filepath
+    server_file_path = Path(tmp)
+    if not os.path.exists(server_file_path):
+        print("404 file not found!")
+        return "",0 
+    # pathが動的で、それがファイルなら読み込んで返す。
+    if os.path.isfile(server_file_path):
+        with open(f"{server_file_path}", "r", encoding="utf-8") as f:
+            content = f.read()
+            print("dynamic")
+            return content, len(content)
+ 
+    print("return 0")
+    return "",0
 
 
 def server():
@@ -35,13 +55,14 @@ def server():
 
                     request = parse_request(raw_request)
                     print(type(request))
-                    pprint(request.headers)
+                    pprint(request.path)
 
-                    if "Referer" in request.headers:
-                        path = request.headers["Referer"].split("/")[3:]
-                        print(path)
+                   # referer = []
+                    #if "Referer" in request.headers:
+                     #   referer = request.headers["Referer"].split("/")[3:]
+                      #  print(referer)
 
-                    content, length = make_response(path)
+                    content, length = make_response(request.path)
 
                     response = (
                         "HTTP/1.1 200 OK\r\n"
@@ -50,6 +71,10 @@ def server():
                         "\r\n"
                         f"{content}"
                     )
+
+                    #print("real res")
+                    #pprint(response)
+
                     client_sock.sendall(response.encode("utf-8"))
                     client_sock.close()
 
