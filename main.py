2070eaf6fab8b2080c7b8ce6f376810b2ffc1c1b
sanbungi <0312pika0201@gmail.com>
2026-01-14 03:58:48 +0900

    httpとhttpsを同時起動

--- main.py
+++ main.py
@@ -69,70 +69,67 @@ def make_response(filepath: str = ".") -> HTTPResponse:
         )
 
 
-def handle_client(client_sock, addr, ssl_context):
+def handle_client(client_sock, addr):
     try:
-        with ssl_context.wrap_socket(client_sock, server_side=True) as client_sock:
-            print(f"Connect by {addr} Thead: {threading.current_thread().name}")
+        keep_alive_timeout = 75
+        client_sock.settimeout(keep_alive_timeout)
 
-            keep_alive_timeout = 5
-            client_sock.settimeout(keep_alive_timeout)
+        while True:
+            try:
+                raw_request = client_sock.recv(1024).decode("utf-8")
+                if not raw_request:
+                    return
 
-            while True:
-                try:
-                    raw_request = client_sock.recv(1024).decode("utf-8")
-                    if not raw_request:
-                        return
+                request = parse_request(raw_request)
+                print("----- request -----")
+                pprint(request)
+                print("-------------------")
 
-                    request = parse_request(raw_request)
-                    print("----- request -----")
-                    pprint(request)
-                    print("-------------------")
+                response = make_response(request.path)
 
-                    response = make_response(request.path)
+                use_keep_alive = get_keep_alive(request)
 
-                    use_keep_alive = get_keep_alive(request)
+                print(f"Use keep-alive: {use_keep_alive}")
 
-                    print(f"Use keep-alive: {use_keep_alive}")
+                # ヘッダーをリスト形式で組み立て、その後にCRLFで結合する
+                headers = [
+                    f"HTTP/1.1 {response.status_code} {get_http_reason_phrase(response.status_code)}",
+                    f"Content-Type: {response.content_type}",
+                    f"Content-Length: {response.content_length}",
+                ]
 
-                    # ヘッダーをリスト形式で組み立て、その後にCRLFで結合する
-                    headers = [
-                        f"HTTP/1.1 {response.status_code} {get_http_reason_phrase(response.status_code)}",
-                        f"Content-Type: {response.content_type}",
-                        f"Content-Length: {response.content_length}",
-                    ]
+                for key, value in response.headers.items():
+                    headers.append(f"{key}: {value}")
 
-                    for key, value in response.headers.items():
-                        headers.append(f"{key}: {value}")
+                if use_keep_alive:
+                    headers.append("Connection: keep-alive")
+                    headers.append(f"Keep-Alive: timeout={keep_alive_timeout}")
+                else:
+                    headers.append("Connection: close")
 
-                    if use_keep_alive:
-                        headers.append("Connection: keep-alive")
-                        headers.append(f"Keep-Alive: timeout={keep_alive_timeout}")
-                    else:
-                        headers.append("Connection: close")
+                # サーバ名を追加
+                headers.append("Server: MyHTTPServer/0.1")
 
-                    # サーバ名を追加
-                    headers.append("Server: MyHTTPServer/0.1")
+                # ヘッダーとコンテンツを送信、バイナリならそのまま送信
+                header_blob = "\r\n".join(headers) + "\r\n\r\n"
+                if isinstance(response.content, bytes):
+                    client_sock.sendall(
+                        header_blob.encode("utf-8") + response.content
+                    )
+                else:
+                    client_sock.sendall(
+                        header_blob.encode("utf-8")
+                        + response.content.encode("utf-8")
+                    )
 
-                    # ヘッダーとコンテンツを送信、バイナリならそのまま送信
-                    header_blob = "\r\n".join(headers) + "\r\n\r\n"
-                    if isinstance(response.content, bytes):
-                        client_sock.sendall(
-                            header_blob.encode("utf-8") + response.content
-                        )
-                    else:
-                        client_sock.sendall(
-                            header_blob.encode("utf-8")
-                            + response.content.encode("utf-8")
-                        )
+                if not use_keep_alive:
+                    print(f"Closing connection with {addr}")
+                    return
 
-                    if not use_keep_alive:
-                        print(f"Closing connection with {addr}")
-                        return
-
-                except socket.timeout:
-                    print(f"Connection with {addr} timed out.")
-                except Exception as e:
-                    print(f"Error keeping connection with {addr}: {e}")
+            except socket.timeout:
+                print(f"Connection with {addr} timed out.")
+            except Exception as e:
+                print(f"Error keeping connection with {addr}: {e}")
     except ssl.SSLError as e:
         print(f"SSL error with client {addr}: {e}")
     except Exception as e:
@@ -140,34 +137,66 @@ def handle_client(client_sock, addr, ssl_context):
 
 
 def server():
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    try:
-        context.load_cert_chain(certfile="server.crt", keyfile="server.key")
-    except Exception as e:
-        print(f"Error loading SSL certificate: {e}")
-        return
+    USE_SSL = True
+    ALSO_HTTP = True
+    HTTP_PORT = 8000
+    HTTPS_PORT = 8443
+    
+    context = None
+    if USE_SSL:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        try:
+            context.load_cert_chain(certfile="server.crt", keyfile="server.key")
+        except Exception as e:
+            print(f"Error loading SSL certificate: {e}")
+            return
 
-    # ipv4, tcp
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
-        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_sock.bind(("", 8000))
+    def run_server_loop(port, ssl_context=None):
+        # ipv4, tcp
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+            server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_sock.bind(("", port))
 
-        # 接続待ち
-        server_sock.listen(5)
+            # 接続待ち
+            server_sock.listen(5)
 
-        print("start server at port 8000")
+            print(f"start server at port {port}")
 
-        with ThreadPoolExecutor(max_workers=10) as executor:
-            try:
+            with ThreadPoolExecutor(max_workers=10) as executor:
                 while True:
                     client_sock, addr = server_sock.accept()
 
-                    executor.submit(handle_client, client_sock, addr, context)
+                    if ssl_context:
+                        try:
+                            client_sock = ssl_context.wrap_socket(
+                                client_sock, server_side=True
+                            )
+                        except ssl.SSLError as e:
+                            print(f"SSL handshake failed with {addr}: {e}")
+                            client_sock.close()
+                            continue
 
-            except KeyboardInterrupt:
-                print("stop server")
-            finally:
-                server_sock.close()
+                    executor.submit(handle_client, client_sock, addr,)
+
+    threads = []
+    
+    if USE_SSL:
+        # HTTPS Server
+        t_https = threading.Thread(target=run_server_loop, args=(HTTPS_PORT, context), daemon=True)
+        t_https.start()
+        threads.append(t_https)
+
+    if ALSO_HTTP or not USE_SSL:
+        # HTTP Server
+        t_http = threading.Thread(target=run_server_loop, args=(HTTP_PORT, None), daemon=True)
+        t_http.start()
+        threads.append(t_http)
+
+    try:
+        for t in threads:
+            t.join()
+    except KeyboardInterrupt:
+        print("stop server")
 
 
 if __name__ == "__main__":
